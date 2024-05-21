bc55f3b2583075aecec2190512f84c732d6aa49a
danielemantovani <daniele.mantovani124@gmail.com>
2024-05-21 15:56:35 +0200

    feat: stampa info di ogni hotel

--- index.php
+++ index.php
@@ -40,7 +40,16 @@
 
     ];
 
+    // foreach ($hotels as $key => $cur_hotel){
+    //     echo"<h4>{$cur_hotel['name']}</h4>";
+    //     echo"<p>{$cur_hotel['description']}</p>";
+    //     echo"<p>{$cur_hotel['parking']}</p>";
+    //     echo"<p>{$cur_hotel['vote']}</p>";
+    //     echo"<p>{$cur_hotel['distance_to_center']}</p>";
+    // }
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -50,6 +59,15 @@
     <title>PHP Hotel</title>
 </head>
 <body>
-    <h1>ciao</h1>
+
+    <?php foreach($hotels as $index => $cur_hotel){ ?>
+        <div>
+            <h4><?php echo $cur_hotel["name"] ?></h4>
+            <p><?php echo $cur_hotel["description"] ?></p>
+            <p><?php echo $cur_hotel["parking"] ?></p>
+            <p>Stelle: <?php echo $cur_hotel["vote"] ?></p>
+            <p>Distanza: <?php echo $cur_hotel["distance_to_center"] ?> km</p>
+        </div>
+    <?php }?>
 </body>
 </html>
